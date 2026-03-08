bf3cdc92fe44cbb3f972665f9b2d452087ec747b
Akash <akashbhattacharjee239@gmail.com>
2026-03-08 13:59:33 +0530

    update session logic and game fix

--- app.py
+++ app.py
@@ -1,33 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import random
 
 app = Flask(__name__)
-
-number = random.randint(1, 100)
-count = 0
+app.secret_key = "akash_2001"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    global count
-    message = ""
-    guess = None
+
+    if "number" not in session:
+        session["number"] = random.randint(1, 100)
+        session["count"] = 0
+
+    message = None
 
     if request.method == "POST":
         guess = int(request.form["guess"])
-        count += 1
+        session["count"] += 1
 
-        if guess == number:
+        if guess == session["number"]:
             message = "win"
-        elif guess > number:
+            number = session["number"]
+            count = session["count"]
+
+            session.clear()
+
+            return render_template(
+                "index.html",
+                message=message,
+                number=number,
+                count=count
+            )
+
+        elif guess > session["number"]:
             message = "lower"
+
         else:
             message = "higher"
 
-    return render_template("index.html",
-                           guess=guess,
-                           count=count,
-                           number=number,
-                           message=message)
+    return render_template(
+        "index.html",
+        message=message,
+        count=session.get("count", 0),
+        number=session.get("number")
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
